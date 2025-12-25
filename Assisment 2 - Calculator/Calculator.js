4c9aa3c10d751e2a42a4e4ca6f139f1a927929a6
Nidhi Agrawal <nidhiagrawal426530@gmail.com>
2025-12-25 21:22:32 +0530

    Update Calculator.js

--- Assisment 2 - Calculator/Calculator.js
+++ Assisment 2 - Calculator/Calculator.js
@@ -1,7 +1,10 @@
-let input = document.getElementbyclass(input);
-let button = document.getElementbyclass(button);
+let input = document.getElementbyclass("input");
+let button = document.getElementbyclass("button");
 
 let arr = Array.from(button);
 arr.forEach(button =>{
-  button.addEventListener('click', (e))})
-            
+  button.addEventListener("click", function(e){
+    console.log("Event type",e.type );
+    e.preventDefault();}
+  });
+  
